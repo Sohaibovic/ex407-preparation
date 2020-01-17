cf6e3b6897bc2c1f9f42ad91474f7b9fdddba6f6
widya <widyaunix@gmail.com>
2020-01-17 13:44:43 +0700

    6

---
+++ exam/timesync.sh
@@ -0,0 +1,8 @@
+ansible-galaxy install linux-system-roles.timesync
+#atau
+yum install rhel-system-roles
+#roles akan terinstall di /usr/share/ansible/roles/
+Referensi:
+* https://access.redhat.com/articles/3050101
+* https://linux-system-roles.github.io/
+* https://galaxy.ansible.com/linux-system-roles/timesync
