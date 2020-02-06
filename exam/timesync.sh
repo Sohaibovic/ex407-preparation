d2a5c7d80be8073008a4f42bce0a60c9b2185169
wid <widyamedia@gmail.com>
2020-02-06 15:06:01 +0700

    time

--- exam/timesync.sh
+++ exam/timesync.sh
@@ -1,7 +1,10 @@
-ansible-galaxy install linux-system-roles.timesync
+ansible-galaxy install linux-system-roles.timesync -p roles/
 #atau
 yum install rhel-system-roles
 #roles akan terinstall di /usr/share/ansible/roles/
+kalau pakai yum berarti di ansible.cfg harus di tambah
+roles_path = /usr/share/ansible/roles:/home/namauser/ansible/roles
+
 Referensi:
 * https://access.redhat.com/articles/3050101
 * https://linux-system-roles.github.io/
